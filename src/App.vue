d37d6b9e54dd37e8dfb1e31a7b92aa3e7230b65e
hirokazukawamuro <110535018+hirokazukawamuro@users.noreply.github.com>
2022-12-15 17:30:39 +0900

    メンバーリスト作成

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,63 @@
+<script>
+export default {
+  data(){
+    return {
+      title: "My New Vue Title",
+      message:"Welcome to Vue",
+      isBlue: true,
+      input:{
+        firstName: '',
+        lastName:'',
+        isMember: true,
+      },
+      users: [
+        {
+          firstName: 'kawamuro',
+          lastName: 'hirokazu',
+          isMember: false,
+        },
+        {
+          firstName: 'roronoa',
+          lastName: 'zoro',
+          isMember: true,
+        },
+        {
+          firstName: 'niko',
+          lastName: 'robin',
+          isMember: true,
+        },
+      ],
+    }
+  },
+  methods: {
+    addUser() {
+      this.users.push(this.input)
+      this.input = {
+        firstName: '',
+        lastName: '',
+        isMember: true,
+      }   
+    },
+  },
+}
+</script>
+
 <template>
-  <h1>My Vue Playground</h1>
-</template>>
+  <h1 :title="message" :class="{ blue: isBlue }">{{ title }}</h1>
+  <input type="text" v-model="input.firstName"/>
+  <input type="text" v-model="input.lastName"/>
+  <input type="checkbox" v-model="input.isMember"/>
+  <button @click="addUser">ユーザー追加</button>
+  <h2>ユーザーのデータ</h2>
+  <div v-for="user in users">
+    <p>Name: {{ user.firstName + ' ' + user.lastName }}</p>
+    <p v-if="user.isMember"> メンバーです</p>
+    <p v-else>メンバーではありません</p>
+  </div>
+</template>
+
+<style>
+.blue {
+  color: blue;
+}
+</style>
